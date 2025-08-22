8fb6513ebc9b98b4832ee6fbf0a60cd8d9bec796
rambojohnj85-hue <rambojohnj85@gmail.com>
2025-08-22 12:45:06 +0200

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
+const App = () => <h1>Leergut Manager läuft 🚀</h1>;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
